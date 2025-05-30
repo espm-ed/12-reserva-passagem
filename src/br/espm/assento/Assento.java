a10dd1930449a72118c8bb28e3d431b985b4be25
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-05-30 14:35:06 -0300

    atualização do projeto

--- src/br/espm/assento/Assento.java
+++ src/br/espm/assento/Assento.java
@@ -7,6 +7,24 @@ public class Assento {
     public Assento(int assento) {
         this.assento = assento;
         this.disponivel = true;
-    }   
+    }
+
+    public int getAssento() {
+        return assento;
+    }
+
+    public void setAssento(int assento) {
+        this.assento = assento;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    } 
+    
+    
 
 }
--- src/br/espm/controle/Controle.java
+++ src/br/espm/controle/Controle.java
@@ -6,14 +6,33 @@ import br.espm.cliente.PessoaJuridica;
 import br.espm.reserva.Reserva;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.TreeSet;
 
 import static java.lang.Integer.parseInt;
 import static java.lang.Double.parseDouble;
 import static javax.swing.JOptionPane.*;
 
 public class Controle {
+
+    private static List<Cliente> listaCliente = new LinkedList<>();
+    private static List<Assento> listaAssento = new LinkedList<>();
+    private TreeSet<Reserva> listaReserva = new TreeSet<>();
+
+    static {
+        // lista de clientes
+        listaCliente.add(new PessoaJuridica("ClienteA", "contatoa@gmail", 741L));
+        listaCliente.add(new PessoaJuridica("ClienteB", "contatob@gmail", 852L));
+        listaCliente.add(new PessoaFisica("ClienteC", "contatoc@gmail", 123L));
+        listaCliente.add(new PessoaFisica("ClienteD", "contatod@gmail", 456L));
+
+        // lista de assentos
+        for(int i = 1; i <= 10; i++) {
+            listaAssento.add(new Assento(i));
+        }
+    }
 
     public void menu() {
         int opcao;
@@ -51,10 +70,33 @@ public class Controle {
     }
 
     private void reservar() {
-        
+        Long identificador = Long.valueOf(showInputDialog("CPF/CNPJ"));
+        Cliente cliente = pesquisarCliente(identificador);
+        if(cliente != null) {
+            int assento = parseInt(showInputDialog(listarAssento()));
+            listaReserva.add(new Reserva(cliente, listaAssento.get(assento), 5000));
+            listaAssento.get(assento).setDisponivel(false);
+        }
     }
 
+    private Cliente pesquisarCliente(Long identificador) {
+        for(Cliente cliente : listaCliente) {
+            if(cliente.getIdentificador().equals(identificador)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
     
+    private String listarAssento() {
+        String aux = "";
+        for(int i = 0; i < listaAssento.size(); i++) {
+            aux += "Assento: " + (i + 1) + " ---> ";
+            aux += listaAssento.get(i).isDisponivel() ? "DISPONÍVEL" : "INDISPONÍVEL";
+            aux += "\n";
+        }
+        return aux;
+    }
 
     private String gerarMenu() {
         String aux = "SISTEMA DE RESERVA DE PASSAGEM AÉREA\n";
